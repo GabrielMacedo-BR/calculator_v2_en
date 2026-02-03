0b98b3ac438b05cac56c7cc8338b144ab32868dc
Gabriel Macedo <gabriel_macedof89@outlook.com>
2026-02-03 19:21:47 -0300

    Initial calculator project with functions

---
+++ calculadora_v2_ptbr.py
@@ -0,0 +1,26 @@
+print ('============ Calculadora Versão 2.0 ================')
+
+
+
+def calculate(number1, number2, operation):
+  if operation == '+':
+    return number1 + number2
+  elif operation == "-":
+    return number1 - number2
+  elif operation == '*':
+    return number1 * number2
+  elif operation =='/':
+    if number2 == 0:
+      return 'Cannot Divide by zero'
+    return number1 / number2
+  
+  else: 
+    return 'Invalid Operation'
+  
+number1 = float(input('Enter a number: '))
+number2 = float(input('Enter another number: '))
+operation = input('What is the arithmetic operation?: ').strip()
+
+result = calculate(number1, number2 , operation)
+
+print('Result: ',result)
